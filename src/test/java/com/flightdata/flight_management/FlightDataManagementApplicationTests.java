385c7d48341a8590e8fd1a073ef24f45c54a2071
Vitor Matosinho Martins <vitor_m_martins@yahoo.com.br>
2025-10-24 19:48:43 -0300

    initialize Flight Data Management Application with basic structure and dependencies

---
+++ src/test/java/com/flightdata/flight_management/FlightDataManagementApplicationTests.java
@@ -0,0 +1,14 @@
+package com.flightdata.flight_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightDataManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+    // Test to ensure the Spring application context loads successfully
+	}
+
+}
