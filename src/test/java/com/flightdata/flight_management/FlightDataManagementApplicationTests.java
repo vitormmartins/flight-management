097e4d36ca9177442027bfbd1b33c2b8d7619cdd
Vitor Matosinho Martins <vitor_m_martins@yahoo.com.br>
2025-10-25 02:54:54 -0300

    refactor application configuration to YAML format and add profiles for Docker and testing

--- src/test/java/com/flightdata/flight_management/FlightDataManagementApplicationTests.java
+++ src/test/java/com/flightdata/flight_management/FlightDataManagementApplicationTests.java
@@ -2,13 +2,15 @@ package com.flightdata.flight_management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class FlightDataManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
-    // Test to ensure the Spring application context loads successfully
+		// Test to ensure the Spring application context loads successfully with application-test.yaml
 	}
 
 }
